c435b39f46c3a09c44302d598946cf48bc147852
Philippe Groarke <hello@philippegroarke.com>
2023-03-18 13:51:54 -0400

    Initial commit.

---
+++ tests/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <fea/terminal/win_term.hpp>
+#include <fea/utils/unused.hpp>
+#include <gtest/gtest.h>
+
+const char* argv0;
+
+
+int main(int argc, char** argv) {
+	// Just test this here so we output utf and it compiles fine on other OSes.
+	auto e = fea::win_utf8_terminal();
+	fea::unused(e);
+
+	argv0 = argv[0];
+
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
